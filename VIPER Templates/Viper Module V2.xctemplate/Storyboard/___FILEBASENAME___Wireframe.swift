a5f28a7c4be107b2b783c0b87b38de33b158c7e1
Anatoliy.V <anatoliy.v@thrive.io>
2019-02-26 16:29:22 +0300

    VIPER

---
+++ VIPER Templates/Viper Module V2.xctemplate/Storyboard/___FILEBASENAME___Wireframe.swift
@@ -0,0 +1,38 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//  VIPER of Thrive
+//
+
+import UIKit
+
+final class ___VARIABLE_moduleName___Wireframe: BaseWireframe {
+
+    // MARK: - Private properties -
+
+    private let _storyboard = UIStoryboard(name: "___VARIABLE_moduleName___VC", bundle: nil)
+
+    // MARK: - Module setup -
+
+    init() {
+        let moduleVC = _storyboard.instantiateViewController(ofType: ___VARIABLE_moduleName___VC.self)
+        super.init(viewController: moduleVC)
+        
+        let interactor = ___VARIABLE_moduleName___Interactor()
+        let presenter = ___VARIABLE_moduleName___Presenter(wireframe: self, view: moduleVC, interactor: interactor)
+        moduleVC.presenter = presenter
+    }
+
+}
+
+// MARK: - Extensions -
+
+extension ___VARIABLE_moduleName___Wireframe: ___VARIABLE_moduleName___WireframeInterface {
+
+    func navigate(to option: ___VARIABLE_moduleName___NavigationOption) {
+    }
+}
